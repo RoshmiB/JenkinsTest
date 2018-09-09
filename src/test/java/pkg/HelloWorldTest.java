6cd457fa2794b705ef44daf320445a535edc8ad5
Roshmi Bhattacharjee <roshmi_bhattacharjee@apple.com>
2018-09-09 11:21:47 +0530

    Jenkins file test

---
+++ src/test/java/pkg/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package pkg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pkg1.HelloWorld;
+
+public class HelloWorldTest {
+	HelloWorld hw = new HelloWorld();
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd2() {
+		assertEquals(7,hw.add(2, 5));
+	}
+}
